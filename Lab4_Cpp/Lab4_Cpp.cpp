bedcc8c3dc307b4212554515f72a8679635c4c30
Admin <Admin@XIAOMI>
2021-10-11 17:25:49 +0300

    Добавьте файлы проекта.

---
+++ Lab4_Cpp/Lab4_Cpp.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int i, j;
+	bool isprime;
+	int a, b;
+	int sum = 0;
+	cin >> a >> b;
+	for (i = a; i < b; i++) {
+		isprime = true;
+		for (j = 2; j <= i / 2; j++)
+			if ((i % j) == 0) isprime = false;
+
+		if (isprime)
+			sum += i * i;
+	}
+	cout << sum;
+
+	return 0;
+}
